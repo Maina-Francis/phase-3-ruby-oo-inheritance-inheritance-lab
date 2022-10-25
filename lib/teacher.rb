599a6d5fc94e69aa1dd2f03710e0141c22d7471a
Francis Maina <sebastianfrancix@gmail.com>
2022-10-26 00:18:30 +0300

    Completed assignment

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,16 @@
-class Teacher
+require_relative "./user.rb"
+# require 'pry'
+
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  def teach 
+    knowledge = KNOWLEDGE.sample(1).join('')
+    knowledge
+  end
 end
+
+# x = Teacher.new
+# x.teach 
+# binding.pry
